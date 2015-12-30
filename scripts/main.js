98858c9525bd3e61b67ce9ecc439b65caaf45e81
paulkass <pkassian@mail.ccsf.edu>
2015-12-29 22:40:34 -0800

    Added basic mouse camera rotation controls

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,12 @@
+//  Some initial variables
+//  **********************
+var ANGLE_OF_ROTATION = Math.PI/200;
+var center = [Math.floor(window.innerWidth/2), Math.floor(window.innerHeight/2)]
+var controlBox = Math.floor(Math.min(window.innerWidth, window.innerHeight)/4);
+var mousePositionX = center[0];
+var mousePositionY = center[1];
+//  **********************
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
@@ -24,7 +33,41 @@ scene.fog = new THREE.FogExp2( 0x000000, 0.0128 );
 renderer.setClearColor( scene.fog.color, 1 );
 
 function render() {
+	var worldVector = camera.getWorldDirection();
+	
+	// Mouse Position -> Rotation Code
+	// -------------------------------
+	if (mousePositionX-center[0]>controlBox) {
+		worldVector.applyAxisAngle(new THREE.Vector3(0,1,0), -ANGLE_OF_ROTATION);
+	}
+	
+	if (mousePositionX-center[0]<-controlBox) {
+		worldVector.applyAxisAngle(new THREE.Vector3(0,1,0), ANGLE_OF_ROTATION);
+	}
+	
+	if (mousePositionY-center[1]>controlBox) {
+		worldVector.applyAxisAngle(new THREE.Vector3(1,0,0), -ANGLE_OF_ROTATION);
+	}
+	
+	if (mousePositionY-center[1]<-controlBox) {
+		worldVector.applyAxisAngle(new THREE.Vector3(1,0,0), ANGLE_OF_ROTATION);
+	}
+	
+	// -------------------------------
+	
+	var cameraVector = new THREE.Vector3(camera.position.x, camera.position.y, camera.position.z);
+	camera.lookAt(worldVector.add(cameraVector));
+	
 	requestAnimationFrame( render );
 	renderer.render( scene, camera );
 }
 render();
+
+$(document).ready(function() {
+	document.captureEvents(Event.MOUSEMOVE)
+	document.onmousemove = function (e) {
+		mousePositionX = e.pageX;
+		mousePositionY = e.pageY;
+	};
+});
+
