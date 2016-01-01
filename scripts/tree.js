f483b72439a098b9743557f8c43d356e6c0d4266
paulkass <pkassian@mail.ccsf.edu>
2016-01-01 14:07:54 -0800

    Fixes #7

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,13 +1,15 @@
 var objectLoader = new THREE.ObjectLoader();
+var treeScale = 0.3;
 
 function placeTreeAtVector(vector) {
 	// Accounting for details in the tree geometry
 	// -------------------------------------------
-	vector.setZ(vector.z+6);
-	vector.setX(vector.x-2.5);
+	vector.setZ(vector.z+6*treeScale);
+	vector.setX(vector.x-2.5*treeScale);
 	// -------------------------------------------
 	objectLoader.load("src/tree-05.json", function(obj) {
 		var tree = getObjectWithPositionVector(obj, vector);
+		tree.scale.set(treeScale, treeScale, treeScale);
 		scene.add(tree);
 	});
 }
