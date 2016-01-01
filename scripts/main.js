5bf238c7aa1bd9dd4325378eed70047cbb03720e
paulkass <pkassian@mail.ccsf.edu>
2015-12-31 16:34:21 -0800

    Revert "Added a constant"
    This reverts commit 9de4b140ab0fbf32330dcaee4eddbb9a20e3ff3e.

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,6 @@ var LEFT_RIGHT_ROTATION_COEFFICIENT = 2;
 var MOVEMENT_SPEED = 0.1; // "Units" per render tick
 var RETICLE_RADIUS = 0.001;
 var WORLD_TO_RETICLE_SCALAR = 0.2;
-var PAN_CONSTANT = 0.5;
 // ---------
 
 var center = [Math.floor(window.innerWidth/2), Math.floor(window.innerHeight/2)];
@@ -44,7 +43,7 @@ var plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.position.y=-0.5;
 scene.add(plane);
 
-var grid = new THREE.GridHelper( 2000, 10 );
+var grid = new THREE.GridHelper( 200, 10 );
 grid.setColors( 0x000000, 0x000000 );
 scene.add( grid );
 
@@ -83,7 +82,7 @@ function pan() {
 	worldProjection.projectOnPlane(new THREE.Vector3(0,1,0)).normalize();
 	var delta = 0;
 	if(mousePositionY != mouseBuffer)
-		delta = mousePositionY > mouseBuffer ? -PAN_CONSTANT : PAN_CONSTANT;
+		delta = mousePositionY > mouseBuffer ? -.5 : .5;
 	camera.position.add(worldProjection.multiplyScalar(delta));
 }
 function rotate() {
