252a588d0c2fee693fb7e6a3c8fe71e68ed5321e
paulkass <pkassian@mail.ccsf.edu>
2015-12-31 23:48:01 -0800

    Increased tree trunk precision

--- scripts/tree.js
+++ scripts/tree.js
@@ -1,5 +1,5 @@
 function returnTreeAtPosition(x,z) {
-	var geometry = new THREE.CylinderGeometry(1,1,100,8,1,false);
+	var geometry = new THREE.CylinderGeometry(1,1,100,32,1,false);
 	var material = new THREE.MeshBasicMaterial({color: 0x964b00});
 	var tree = new THREE.Mesh(geometry, material);
 	tree.position.y = 0;
