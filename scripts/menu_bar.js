5e3e8e10287456f1356b317b7462b69b57bfe412
paulkass <pkassian@mail.ccsf.edu>
2016-01-04 10:31:04 -0800

    Added more styling to the menu bar

--- scripts/menu_bar.js
+++ scripts/menu_bar.js
@@ -1,10 +1,15 @@
+var horizontalMarginFactor = 0.1;
+var heightFactor = 0.3;
 
 $("#menu_bar").tabs({
 	collapsible: true
 });
 $("#menu_bar").hide();
 $("#menu_bar").css({
-	"width": 0.9*window.innerWidth
+	"right": horizontalMarginFactor*window.innerWidth,
+	"left": horizontalMarginFactor*window.innerWidth,
+	"width": (1-2*horizontalMarginFactor)*window.innerWidth,
+	"height": heightFactor*window.innerHeight
 });
 
 function toggleMenuBar() {
