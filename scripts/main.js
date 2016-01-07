080402c2f3451285131487b3045d896a875752be
clarencechen <clarencechenct@gmail.com>
2016-01-06 19:05:13 -0800

    removed gizmo controls

--- scripts/main.js
+++ scripts/main.js
@@ -283,7 +283,6 @@ function updateSelectedObjectPosition() {
 			var heldPos = cameraVector.add(worldVector.multiplyScalar(WORLD_TO_PLANK_SCALAR));
 			selectedObject.position.set(heldPos.x, heldPos.y, heldPos.z);
 			//selectedObject.lookAt(worldVector.add(cameraVector));
-			objectControls.update();
 
 			break;
 		case "tree":
