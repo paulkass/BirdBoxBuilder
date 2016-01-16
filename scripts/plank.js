b7b0d2cc6eb96fc4fdbd122efad1e52981024d53
paulkass <pkassian@mail.ccsf.edu>
2016-01-15 17:00:07 -0800

    Added a method to the plank cla

--- scripts/plank.js
+++ scripts/plank.js
@@ -6,10 +6,26 @@ function Plank(w,l,h) {
 		this.geometry.vertices.forEach( function(v) {
 			arr.push(v.clone().applyMatrix4(this.matrixWorld));
 		});
+		return arr;
 	};
 	
 	this.getSideClosestToPoint = function(point) {  // point is expected to be of type THREE.Vector3
-	
+		var sortedVectorArr = [];
+		var unsortedVectorArr = this.getVectorsInWorldContext();
+		for (var trol = 0; trol<4; trol++) {
+			var shortestVector = new THREE.Vector3(Global.Infinity, Global.Infinity, Global.Infinity);
+			var shortestVectorIndex = 0;
+			unsortedVectorArr.forEach(function (v, i) {
+				if (v.clone().sub(point).length() < shortestVector.clone().sub(point).length()) {
+					shortestVector.copy(v);
+					shortestVectorIndex = i;
+				}
+			});
+			unsortedVectorArr.splice(shortestVectorIndex, 0);
+			sortedVectorArr.push(shortestVector.clone());
+		}
+		
+		return sortedVectorArr;
 	}; 
 }
 
