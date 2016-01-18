5b0bb8402dd662b19380d653f9f0cd4caf60a9a4
paulkass <pkassian@mail.ccsf.edu>
2016-01-17 19:31:07 -0800

    Removed some old code

--- scripts/snap.js
+++ scripts/snap.js
@@ -4,7 +4,7 @@ function snap(plank1) {
 	scene.children.forEach(function (plank2){
 		if(plank2.name.includes("plank") && plank2.id != plank1.id)
 		{
-			// XZ plane snap
+			// Old Code
 			// *************
 			plank1.geometry.computeBoundingSphere();
 			plank2.geometry.computeBoundingSphere();
@@ -16,7 +16,7 @@ function snap(plank1) {
 			var errorMargin = plank1.geometry.boundingSphere.radius +plank2.geometry.boundingSphere.radius +0.25;
 			if( d.length() < errorMargin)
 			{
-				console.log("ready to snap");
+				//console.log("ready to snap");
 				if(d.z > d.x && d.z > -d.x)//+z section of plane
 				{
 					d.setZ(box1.z +box2.z);
@@ -37,42 +37,7 @@ function snap(plank1) {
 			}
 			
 			// ************
-			
-			// Y snap
-			// ------------
-			
-			// plank1.updateMatrixWorld();
-// 			plank2.updateMatrixWorld();
-// 			
-// 			var plank1Vertices = [];
-// 			var plank2Vertices = [];
-// 			
-// 			plank1.geometry.vertices.forEach( function(v) {
-// 				// This function should be efficianized
-// 				plank1Vertices.push(v.clone().applyMatrix4(plank1.matrixWorld));
-// 			});
-// 			
-// 			plank2.geometry.vertices.forEach( function(v) {
-// 				plank2Vertices.push(v.clone().applyMatrix4(plank2.matrixWorld));
-// 			});
-			
-			// function checkVertices() {
-// 				plank1Vertices.forEach(function(v) {
-// 					plank2Vertices.forEach(function(u) {
-// 						var diff = u.clone().sub(v);
-// 						if (diff.length() <= snapDistance && diff.length() >= 0.3*snapDistance) {
-// 							// var quat = new THREE.Quaternion().setFromUnitVectors(v.clone().normalize(), u.clone().normalize());
-// // 							var matrix = new THREE.Matrix4().makeRotationFromQuaternion(quat);
-// // 							plank1.applyMatrix(matrix);
-// 							console.log("snapping");
-// 							plank1.position.add(diff);
-// 						
-// 						}
-// 					});
-// 				});
-// 			}
-			
-			// ------------
+		
 			
 			var closest1 = plank1.getSideClosestToPoint(plank2.position)[0];
 			var closest2 = plank2.getSideClosestToPoint(plank1.position)[0];
